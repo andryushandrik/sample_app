807af531beb8c2a098684501f4961ca47499c63e
Andrey <andreyprohoreno@gmail.com>
2024-05-23 11:00:27 +0300

    Initial commit

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+
+  test "should get home" do
+    get 'http://127.0.0.1:3000'
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get help" do
+    get 'http://127.0.0.1:3000/help'
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,38 @@
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+
+module ActiveSupport
+  class TestCase
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors, with: :threads)
+
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
+    include ApplicationHelper
+    # Add more helper methods to be used by all tests here...
+
+    # Возвращает true, если тестовый пользователь осуществил вход.
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
+
+    # Осуществляет вход тестового пользователя
+    def log_in_as(user, options = {})
+      password    = options[:password]    || 'password'
+      remember_me = options[:remember_me] || '0'
+      if integration_test?
+        post 'http://127.0.0.1:3000/login', params: { session: { email:       user.email,
+                                                                 password:    password,
+                                                                 remember_me: remember_me } }
+      else
+        session[:user_id] = user.id
+      end
+    end
+
+    # Возвращает true внутри интеграционных тестов
+    def integration_test?
+      defined?(follow_redirect!)
+    end
+  end
+end
